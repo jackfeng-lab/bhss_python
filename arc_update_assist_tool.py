0eb227b849ad591fbd9450487e08161e1587a4b8
feng huajie <fhj072199@163.com>
2023-06-08 10:37:27 +0800

    [Version 1]
    	use table to table sequence compare

---
+++ arc_update_assist_tool.py
@@ -0,0 +1,139 @@
+import os
+import csv
+import shutil
+
+block_d1 = []
+block_d2 = []
+diff_flag = False
+number_of_table_1 = 0
+number_of_table_2 = 0
+table_name = ""
+
+#读取table数
+def read_table_count():
+    global number_of_table_1
+    global number_of_table_2
+
+    with open("dependency_1.csv",'r',encoding='UTF-8') as load_input_1:
+        ereader_1 = csv.reader(load_input_1)
+        for row_list in ereader_1:
+            for i in row_list:
+                if i[0:5] == "Table":
+                    number_of_table_1 += 1
+        load_input_1.close()
+    with open("dependency_2.csv",'r',encoding='UTF-8') as load_input_2:
+        ereader_2 = csv.reader(load_input_2)
+        for row_list in ereader_2:
+            for i in row_list:
+                if i[0:5] == "Table":
+                    number_of_table_2 += 1
+        load_input_2.close()
+    
+    return number_of_table_1 if number_of_table_1>number_of_table_2 else number_of_table_2
+
+#读取dependency 1中的一个表
+def read_block_d1(roll_num_1):
+    global block_d1
+    global table_name
+    flag = False
+    local_seek_1 = 0
+    with open("dependency_1.csv",'r',encoding='UTF-8') as load_input_1:
+        ereader_1 = csv.reader(load_input_1)
+        for row_list in ereader_1:
+            for i in row_list:
+                if i[0:5] == "Table":
+                    table_name = i[7:-11]
+                    flag = True
+                    local_seek_1 += 1
+                    block_d1.clear()
+                if row_list[0] == "" and local_seek_1 == roll_num_1:
+                    flag = False
+                    # print (block_d1)
+                    return
+            if flag == True and local_seek_1 == roll_num_1:
+                block_d1.append(row_list)
+        load_input_1.close()
+
+#读取dependency 2中的一个表
+def read_block_d2(roll_num_2):
+    global block_d2
+    flag = False
+    local_seek_2 = 0
+    with open("dependency_2.csv",'r',encoding='UTF-8') as load_input_2:
+        ereader_2 = csv.reader(load_input_2)
+        for row_list in ereader_2:
+            for i in row_list:
+                if i[0:5] == "Table":
+                    flag = True
+                    local_seek_2 += 1
+                    block_d2.clear()
+                if row_list[0] == "" and local_seek_2 == roll_num_2:
+                    flag = False
+                    # print (block_d2)
+                    return
+            if flag == True and local_seek_2 == roll_num_2:
+                block_d2.append(row_list)
+        load_input_2.close()
+
+#检查表1 和表2是否存在差异
+def check_diff_point():
+    global block_d1
+    global block_d2
+    global diff_flag
+
+    for line in block_d2:
+        if line not in block_d1:
+            diff_flag = True
+            return
+    for line in block_d1:
+        if line not in block_d2:
+            diff_flag = True
+            return
+
+#如果存在任何差异，输出整张Table
+def write_to_file(f1):
+    global diff_flag
+    global block_d2
+    null_line = []
+
+    writer = csv.writer(f1)
+    if diff_flag is True:
+        for line in block_d2:
+            writer.writerow(line)
+        writer.writerow(null_line)
+        diff_flag = False
+
+#复制变更组件的xml文件到新目录下
+def copy_diff_xml():
+    global diff_flag
+    global table_name
+
+    if diff_flag is True:
+        print (table_name)
+        if os.path.exists("Diff//" + table_name):
+            shutil.rmtree("Diff//" + table_name)
+        
+        if not os.path.exists(table_name):
+            os.makedirs("Diff//" + table_name)
+
+        shutil.copyfile("Output//Dependency " + table_name + ".xml", "Diff//" + table_name +"//Dependency " + table_name + ".xml")
+        shutil.copyfile("Output//Interface " + table_name + ".xml", "Diff//" + table_name +"//Interface " + table_name + ".xml")
+
+#运行区域
+if __name__== "__main__" : 
+    roll_num_1 = 0
+    roll_num_2 = 0
+
+    f1 = open("diff_point.csv", 'w', newline='',encoding='UTF-8')
+    number_of_table = read_table_count()
+
+    #每个Table逐次对比
+    for n in range(number_of_table):
+        read_block_d1(roll_num_1)
+        read_block_d2(roll_num_2)
+        roll_num_1 += 1
+        roll_num_2 += 1
+        check_diff_point()
+        copy_diff_xml()
+        write_to_file(f1)
+    f1.close()
